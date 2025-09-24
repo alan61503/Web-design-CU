9a65bc4cf70be59c55e14c11c2a968fba56b87f9
alan61503 <alanchrisdisilva@gmail.com>
2025-09-25 00:49:01 +0530

    .

---
+++ lab9.js
@@ -0,0 +1,11 @@
+for (let i = 5; i <= 15; i++) {
+  console.log(i);
+}
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 === 0) {
+    console.log(i + " is Even");
+  } else {
+    console.log(i + " is Odd");
+  }
+}
